9c48d36cf93fb197b6dab8da6f801b5d3029598d
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-02-21 10:52:37 -0300

    refactor: renomeando função

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,10 +1,9 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
       
-const swiper__principal = new Swiper('.slide-content', {
+const swiper = new Swiper('.slide-content', {
     spaceBetween: 80,
 
     pagination: {
         el: '.swiper-pagination',
-        dynamicBullets: true,
     }
 });
